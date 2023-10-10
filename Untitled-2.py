ebbade6bbd90ca95ed8249dba831a4cc4852e56d
Dri23 <147421742+Dri23@users.noreply.github.com>
2023-10-10 11:50:19 -0300

    Create Untitled-2.py

---
+++ Untitled-2.py
@@ -0,0 +1,10 @@
+n1 = int(input("Entre com a nota 1: "))8
+n2 = int(input("Entre com a nota 2: "))
+n3 = int(input("Entre com a nota 3: "))
+n4 = int(input("Entre com a nota 4: "))
+
+media = float(n1+n2+n3+n4)/4
+
+print(f'A media e:{media}')
+
+
